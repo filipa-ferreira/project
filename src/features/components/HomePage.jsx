358a8609152f7ae346010870580f43b25c1e8eca
Filipa Ferreira <info@filipaferreira.dev>
2023-05-11 14:50:44 +0100

    aletração botão+visual

--- src/features/components/HomePage.jsx
+++ src/features/components/HomePage.jsx
@@ -50,34 +50,34 @@ export default function HomePage() {
   return (
     <section>
     <header>
-      <img className='logo' src="images/logo.png" alt="logo" />
+      <img className='logo' src="/images/logo.png" alt="logo" />
       <h1>&lt;Filipa Ferreira/&gt;</h1>
       <Menu/>
     </header>
     <section className='grid'>
-      <article className='tag'>
+      <article className='tag initial'>
         <p>&lt;&gt;</p>
       </article>
       <article className='c2xr1 text'>
         <p ref={textRef}>Durante a minha vida profissional tanto na indústria como na gestão de serviços, deparei-me constantemente com a necessidade de otimizar processos e utilizar a tecnologia para alcançar melhores resultados.</p>
       </article>
       <figure className='c2xr1'>
-        <img src="images/computer.jpg" alt="computer"/>
+        <img src="/images/computer.jpg" alt="computer"/>
       </figure>
       <figure className='c1xr2'>
-        <img src="images/vscode.jpg" alt="vscode"/>
+        <img src="/images/vscode.jpg" alt="vscode"/>
       </figure>
       <article className='c1xr2 welcome'>
         <p className='title'>Bem-vindo!</p>
       </article>
       <figure className='c2xr2'>
-        <img src="images/programming.jpg" alt="to-program"/>
+        <img src="/images/programming.jpg" alt="to-program"/>
       </figure>
       <article className='c2xr1 text'>
         <p ref={textRef}>Surgindo a necessidade de colaborar com desenvolvedores de software numa indústria conserveira para criar uma aplicação de controlo do processo e eficiência do mesmo, nasceu a minha curiosidade pela área.</p>
       </article>
-      <figure>
-        <img src="images/steps.jpg" alt="steps"/>
+      <figure className='www'>
+        <img src="images/www.jpg" alt="steps"/>
       </figure>
       <article className='c2xr1 text'>
         <p ref={textRef}>A fase pandémica, onde a tecnologia foi o nosso maior suporte a todos os níveis, foi o gatilho para procurar mais informação e conhecimento na área. Assim, propus-me a formação especializada para aprimorar as minhas habilidades e conhecimentos.</p>
@@ -86,13 +86,12 @@ export default function HomePage() {
         <p><FontAwesomeIcon icon={faEnvelope} /> info@filipaferreira.dev</p>
         <p><FontAwesomeIcon icon={faPhone} /> +351 93 83 22 319</p>
       </article>
+
       {/* <article>
       <Link className='form-grid' to="/contactForm">Formulário de contato<FontAwesomeIcon icon={faHandPointer} /></Link>
         <p></p>
-      </article>
-      <article className='c2xr1 welcome'>
-        <h1 className='title'>Obrigada pela atenção!</h1>
       </article> */}
+      
       <article className='tag'>
         <p>&lt;/&gt;</p>
       </article>
--- src/features/components/Menu.jsx
+++ src/features/components/Menu.jsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import '../../styles/menu.css';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 
 export default function Menu() {
@@ -25,9 +26,17 @@ export default function Menu() {
       </Navbar.Toggle>
       <Navbar.Collapse id="responsive-navbar-nav" className={menuOpen ? 'show' : ''}>
         <Nav className="mr-auto">
-          <Nav.Link href="#sobre-mim">Sobre mim</Nav.Link>
+          {/* <Nav.Link href="#sobre-mim">Sobre mim</Nav.Link>
           <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-          <Nav.Link href="#contato">Contato</Nav.Link>
+          <Nav.Link className='form-grid' to="/contactForm">Contato</Nav.Link> */}
+          <Link className='btn' to="#">Sobre mim</Link>
+          <Link className='btn' to="#">Portfolio</Link>
+          <Link className='btn' to="/contactForm">Contato</Link>
+
+          
+      {/* <Link className='form-grid' to="/contactForm">Formulário de contato</Link> */}
+        
+
         </Nav>
       </Navbar.Collapse>
     </Navbar>
